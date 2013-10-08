e6069fdd70c0f64d13325f158e6f2b7f26e4deca
Ihar Hancharenka <iigoncharenko@mail.ru>
2013-10-08 18:09:16 +0300

    autocommit 2013-10-08 18:09:12

--- cmd/pl/jvm/yadisk-up.bat
+++ cmd/pl/jvm/yadisk-up.bat
@@ -4,6 +4,6 @@ call vars-java-p.bat
 call vars-jtools.bat
 call vars-ydisk.bat
 
-java -jar %JTOOLS_HOME%/webdav_sync/webdav_sync1_1_2.jar -r -up -u https://%YDISK_LOGIN%:%YDISK_PWD%@webdav.yandex.ru/Backup/ -d %YDISK_LOCAL_BACKUP_DIR%
+java -jar %JTOOLS_HOME%/webdav_sync/webdav_sync1_1_4.jar -r -up -u https://%YDISK_LOGIN%:%YDISK_PWD%@webdav.yandex.ru/Backup/ -d %YDISK_LOCAL_BACKUP_DIR%
 :: -reset
 endlocal
