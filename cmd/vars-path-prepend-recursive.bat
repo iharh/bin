3a807169483d42a7e6f1aa4664fd5a56d056b935
Ihar Hancharenka <iigoncharenko@mail.ru>
2013-10-17 16:57:55 +0300

    autocommit 2013-10-17 16:57:51

--- cmd/vars-path-prepend-recursive.bat
+++ cmd/vars-path-prepend-recursive.bat
@@ -1,4 +1,4 @@
 @echo off
-:: TODO: filter dots by   findstr /V /L .
-for /d /r %1 %%i in (*) do call %~dp0vars-path-prepend.bat %%i
+::for /d /r %1 %%i in (*) do call %~dp0vars-path-prepend.bat %%i
+for /f "delims=" %%i in ('dir /A:D /B /S %~dp0 ^| findstr /V /L .') do call %~dp0vars-path-prepend.bat %%i
 call %~dp0vars-path-prepend.bat %1
