f08be7ab9a39684afa71fa2682c260048d40fdec
Ihar Hancharenka <iigoncharenko@mail.ru>
2013-10-30 18:11:45 +0300

    autocommit 2013-10-30 18:11:34

--- obsolete/pl/haskell/pipes-simple/g.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-call vars-haskell.bat
-set GHC_PACKAGE_PATH=%HASKELL_CABAL_DEV_SANDBOXES_HOME%\pipes\cabal-dev\packages-%_HASKELL_COMPILER_VER%.conf;
-
-ghci -x "hs" a.hs
-
-endlocal
--- obsolete/pl/node.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-for %%i in ("%~dp0..\JavaScript\nodejs") do (set NODEJS_HOME=%%~fi)
-set PATH=%PATH%;%NODEJS_HOME%
-
-node.exe %*
-
-endlocal
