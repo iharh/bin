1325b6fb0ae1fffb50d680270551294f2f48a09a
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-01-31 17:36:11 +0300

    autocommit 2014-01-31 17:36:06

--- cmd/util/vim/gvim.bat
+++ cmd/util/vim/gvim.bat
@@ -2,6 +2,7 @@
 setlocal
 call vars-vim.bat
 start "dummy" /b "%VIM_EXE_DIR%\gvim.exe" --remote-silent %*
+:: -V20... -V8D:\vimlog.txt 
 :: if NOT .%1==.-f goto noforkarg
 :::noforknt
 ::start "dummy" /b /wait "%VIM_EXE_DIR%\gvim.exe"  %*
