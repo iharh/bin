db3c3bad51a2e83ce7b4296a9e6ba705aad62c96
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-02-27 09:49:49 +0300

    autocommit 2014-02-27 09:49:46

--- bgh.bat
+++ bgh.bat
@@ -1,17 +1,12 @@
 @echo off
 setlocal
-pushd %~dp0
-
 call vars-ecm-p.bat
 
 call ss-copydf.bat
-
-call git-ap.bat
+call bin-ap.bat 
 
 call wrk-backup.bat
 call yadisk-up.bat
 ::call bk-up.bat
-
-popd
 endlocal
 
