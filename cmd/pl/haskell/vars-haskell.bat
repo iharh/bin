792bc2f19f688369956359cd09b3ef6dac04f30c
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-05-03 18:22:09 +0300

    autocommit 2014-05-03 18:22:06

--- cmd/pl/haskell/vars-haskell.bat
+++ cmd/pl/haskell/vars-haskell.bat
@@ -10,10 +10,3 @@ set _HASKELL_HOME=%_HASKELL_BASE%\%_HSAKELL_VER%
 
 set HASKELL_CABAL_CACHE_HOME=%_HASKELL_BASE%\cabal
 set HASKELL_CABAL_SANDBOXES_HOME=%_HASKELL_BASE%\cabal-sandboxes
-
-::for %%i in ("%~dp0..\GTKPlus") do (set _GTK_PLUS_HOME=%%~fi)
-
-
-::set GHC_PACKAGE_PATH=%~dp0..\Haskell\prj\mypipes\cabal-dev\packages-7.4.2.conf;
-:: --package-conf=FILE
-
