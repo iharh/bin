2eb1204e99fd686882be2e3f9904b4c7cab79b27
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-09-30 17:48:25 +0300

    autocommit 2014-09-30 17:48:23

--- ems.bat
+++ ems.bat
@@ -1,18 +1,16 @@
 @echo off
 setlocal
-::call vars-ems-p.bat
-
-set MSYS_ROOT=D:\msys64
-
+call vars-ems-p.bat
 
 set MSYSTEM=MSYS
 rem To activate windows native symlinks uncomment next line
-set MSYS=winsymlinks:nativestrict
+set MSYS=winsymlinks
+::set MSYS=winsymlinks:nativestrict
 rem Set debugging program for errors
 rem set MSYS=error_start:%WD%../../mingw32/bin/qtcreator.exe|-debug|<process-id>
 set MSYSCON=mintty.exe
 
-start %MSYS_ROOT%\usr\bin\mintty -i /msys2.ico /usr/bin/bash --login %*
+start %MSYS_ROOT%\usr\bin\mintty -i /msys2.ico /usr/bin/zsh --login %*
 
 ::start %MSYS_ROOT%\bin\mintty.exe --exec "%MSYS_ROOT%\bin\bash.exe" --login -i
 :: -c %USERPROFILE%\.minttyrc
--- vars-ems-p.bat
+++ vars-ems-p.bat
@@ -1,9 +1,11 @@
 @echo off
-call %~dp0cmd\vars-path-prepend.bat %~dp0cmd %~dp0cmd/util
-call %~dp0cmd\util\repo\git\vars-git.bat 
+set MSYS_ROOT=D:\msys64
 
-set MINGW_ROOT=%UTILS_ROOT%\Shell\MinGW
-set MSYS_ROOT=%MINGW_ROOT%\msys\1.0
+::call %~dp0cmd\vars-path-prepend.bat %~dp0cmd %~dp0cmd/util
+::call %~dp0cmd\util\repo\git\vars-git.bat 
 
-call %~dp0cmd\vars-path-prepend.bat %GIT_ROOT%\cmd
+::set MINGW_ROOT=%UTILS_ROOT%\Shell\MinGW
+::set MSYS_ROOT=%MINGW_ROOT%\msys\1.0
+
+::call %~dp0cmd\vars-path-prepend.bat %GIT_ROOT%\cmd
 
