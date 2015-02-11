3f61ac3af3406d39225b71117df102d24d28fc74
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-02-11 12:58:30 +0300

    autocommit 2015-02-11 12:58:28

--- ext-vim.bat
+++ ext-vim.bat
@@ -1,6 +1,12 @@
 @echo off
 setlocal
-call vars-ecm-p.bat
-call vim.bat -- %*
+::findstr hangs sometimes
+::call vars-ecm-p.bat
+::call vim.bat -- %*
+
+::set VIM_EXE_DIR=D:\dev\Utils\Shell\Vim\vim74
+::"%VIM_EXE_DIR%\vim.exe" -- %*
+
+"D:\dev\Utils\Shell\Vim\vim74\vim.exe" -- %*
 endlocal
 
