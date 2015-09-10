4892c4317c588aae11d719b41365318969bf9296
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-09-10 18:29:21 +0300

    autocommit 2015-09-10 18:29:18

---
+++ cmd/pl/jvm/sbt-du.bat
@@ -0,0 +1,3 @@
+@call sbt.bat dependencyUpdates
+:: project/sbt-updates.sbt:
+:: addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.9")
--- cmd/pl/jvm/sbt8.bat
+++ cmd/pl/jvm/sbt8.bat
@@ -4,7 +4,8 @@ call vars-java8.bat
 call vars-scala.bat
 set SBT_HOME=%SCALA_TOOLS_HOME%/sbt
 
-set SBT_OPTS=-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=128m -Dsbt.log.format=true
+set SBT_OPTS=-Xms1024M -Xmx7G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=128m -Dsbt.log.format=true
+:: -Xmx1536M
 
 %JAVA_HOME%/bin/java %SBT_OPTS% -jar "%SBT_HOME%/sbt-launch.jar" "-Dsbt.boot.directory=%SBT_HOME%/boot" "-Dsbt.ivy.home=%SBT_HOME%/.ivy2" %*
 endlocal
