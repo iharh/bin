21674ee095f64b7d44a7d7654b71d4638afa3d14
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-09-11 19:06:15 +0300

    autocommit 2015-09-11 19:06:01

--- cmd/pl/jvm/sbt7.bat
+++ cmd/pl/jvm/sbt7.bat
@@ -4,7 +4,8 @@ call vars-java7.bat
 call vars-scala.bat
 set SBT_HOME=%SCALA_TOOLS_HOME%/sbt
 
-set SBT_OPTS=-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=128m -Dsbt.log.format=true
+set SBT_OPTS=-Xms8G -Xmx8G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=128m -Dsbt.log.format=true
+::-Xms512M -Xmx1536M
 set SBT_OPTS=-XX:MaxPermSize=256M %SBT_OPTS%
 
 %JAVA_HOME%/bin/java %SBT_OPTS% -jar "%SBT_HOME%/sbt-launch.jar" "-Dsbt.boot.directory=%SBT_HOME%/boot" "-Dsbt.ivy.home=%SBT_HOME%/.ivy2" %*
