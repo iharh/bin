96b565756fe6c16f55eef50bf4b880f9e1ad3535
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-10-21 12:51:12 +0300

    autocommit 2015-10-21 12:51:10

--- cmd/util/shell/vars-wcd.bat
+++ cmd/util/shell/vars-wcd.bat
@@ -3,5 +3,5 @@ set WCDSCAN=%~dp0.wcd
 ::D:\dev\bin;D:\dev\notes;D:\dev\prj
 ::D:\clb\src\platform;D:\clb\src\platform\fx\fx
 set WCDEXCLUDE=*/.git;*/.svn
-set WCDHOME=D:\dev\wrk\.wcd
+set WCDHOME=%~dp0.wcd
 
