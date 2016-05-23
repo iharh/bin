225e9a467643062b0f681d5d3050690abcbefd9c
Ihar Hancharenka <iigoncharenko@mail.ru>
2016-05-23 17:43:03 +0300

    autocommit 2016-05-23 17:42:22

--- cmd/pl/jvm/sbt8.bat
+++ cmd/pl/jvm/sbt8.bat
@@ -7,6 +7,6 @@ set SBT_HOME=%SCALA_TOOLS_HOME%/sbt
 set SBT_OPTS=-Xms1G -Xmx8G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=128m -Dsbt.log.format=true -Dfile.encoding=UTF-8
 :: -Xmx1536M
 
-%JAVA_HOME%/bin/java %SBT_OPTS% -jar "%SBT_HOME%/sbt-launch.jar" "-Dsbt.boot.directory=%SBT_HOME%/boot" "-Dsbt.ivy.home=%SBT_HOME%/.ivy2" %*
+%JAVA_HOME%/bin/java %SBT_OPTS% -jar "%SBT_HOME%/bin/sbt-launch.jar" "-Dsbt.boot.directory=%SBT_HOME%/boot" "-Dsbt.ivy.home=%SBT_HOME%/.ivy2" %*
 endlocal
 
