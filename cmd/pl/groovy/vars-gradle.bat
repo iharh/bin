a3ea81aa1722e22a3a8e949c788afdfc0bd3157d
Ihar Hancharenka <iigoncharenko@mail.ru>
2016-10-26 17:14:02 +0300

    autocommit 2016-10-26 17:13:38

--- cmd/pl/groovy/vars-gradle.bat
+++ cmd/pl/groovy/vars-gradle.bat
@@ -1,5 +1,4 @@
 @echo off
 call vars-java8.bat
-set GRADLE_HOME=%PL_ROOT%\groovy\tools\gradle-3.0
-::2.14
+set GRADLE_HOME=%PL_ROOT%\groovy\tools\gradle-3.1
 set GRADLE_USER_HOME=%PL_ROOT%\groovy\tools\.gradle
--- cmd/pl/jvm/vars-java8.bat
+++ cmd/pl/jvm/vars-java8.bat
@@ -1,4 +1,4 @@
 @echo off
 call vars-pl.bat
-set JAVA_HOME=%PL_ROOT%\java\jdk64\jdk1.8.0_60
+set JAVA_HOME=%PL_ROOT%\java\jdk64\jdk1.8.0_112
 
