f03cb71aead1f6228f70cbd51b52a444c2bc155b
Ihar Hancharenka <iigoncharenko@mail.ru>
2016-11-22 17:25:53 +0300

    autocommit 2016-11-22 17:25:51

--- cmd/util/vim/vars-vim.bat
+++ cmd/util/vim/vars-vim.bat
@@ -1,4 +1,5 @@
 @echo off
 call vars-utils.bat
-set VIM_EXE_DIR=%UTILS_ROOT%\Shell\Vim\vim74
+set VIM_EXE_DIR=%UTILS_ROOT%\Shell\Vim\vim80-kaoriya-win64
+::vim74
 ::call vars-path-append.bat %VIM_EXE_DIR%
