c1d80409a9e98d35ede47d697806bf1e56e363e1
Ihar Hancharenka <iigoncharenko@mail.ru>
2016-12-21 17:29:37 +0300

    autocommit 2016-12-21 17:29:36

--- cmd/clb/clb-b-cmn.bat
+++ cmd/clb/clb-b-cmn.bat
@@ -124,8 +124,8 @@ call clb-gri-cmn.bat 2>%GROK_LOG%
 :skipIndexing
 
 if %1.==nobuild. goto lDone
-::set CLB_DEFS_B=%CLB_DEFS_B% -Dinstaller.revision=%new_rev%
-call gradlew.bat build -Pbuild.type=nlp -Dnlp.workspace=%CLB_SVN_SRC_ROOT% >>%BUILD_LOG% 2>&1
+set CLB_DEFS_G=-Pmvn.repo=http://epbygomw0039t1.gomel.epam.com:8099/nexus/content/groups/public
+call gradlew.bat build -Pbuild.type=nlp -Pnlp.workspace=%CLB_SVN_SRC_ROOT% %CLB_DEFS_G% >>%BUILD_LOG% 2>&1
 ::continuous
 
 :lDone
