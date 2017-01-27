64df77d769921e6689511891aa0491fa014004e9
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-01-27 17:42:03 +0300

    autocommit 2017-01-27 17:41:54

--- emingw.bat
+++ emingw.bat
@@ -3,6 +3,7 @@ setlocal
 call vars-ems-p.bat
 
 set MSYSTEM=MINGW64
+::set MSYS2_PATH_TYPE=strict
 set "CONTITLE=MinGW x64"
 set "WD=%MSYS_ROOT%\usr\bin\"
 set "ComEmuCommand=%MSYS_ROOT%\opt\bin\ConEmu"
