357a4a06ab5d0f780bb9c195f058e1ab26da6ae0
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-08-22 10:52:53 +0300

    autocommit 2017-08-22 10:52:52

--- cmd/util/repo/git/git-repo-up.bat
+++ cmd/util/repo/git/git-repo-up.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 call vars-git-repo.bat
-set LOG_FILE=%REPO_ROOT%/git_up_%val_dt%.log
+set LOG_FILE=%REPO_ROOT%/git-up_%val_dt%.log
 for /f "delims=" %%a in (%GIT_DIRS_FILE%) do call _git-up.bat %%a
 endlocal
