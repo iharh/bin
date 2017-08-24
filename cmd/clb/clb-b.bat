e89cfe178823a89dafaef570b9baa8d9742df981
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-08-24 18:18:55 +0300

    autocommit 2017-08-24 18:18:53

--- cmd/clb/clb-b.bat
+++ cmd/clb/clb-b.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set CTAGS_SRC=1
-set BUILD_FXLP=1
+::set BUILD_FXLP=1
 call vars-clb-cur.bat
 call clb-b-cmn.bat
 endlocal
--- cmd/pl/groovy/vars-gradle.bat
+++ cmd/pl/groovy/vars-gradle.bat
@@ -1,4 +1,4 @@
 @echo off
 call vars-java8.bat
-set GRADLE_HOME=%PL_ROOT%\groovy\tools\gradle-4.0
+set GRADLE_HOME=%PL_ROOT%\groovy\tools\gradle-4.1
 set GRADLE_USER_HOME=%PL_ROOT%\groovy\tools\.gradle
