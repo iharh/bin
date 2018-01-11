b98d9dfc57d50bf4a9caf1cb6bffedf2e18bf7bb
Ihar Hancharenka <iigoncharenko@mail.ru>
2018-01-11 18:18:04 +0300

    autocommit 2018-01-11 18:18:01

--- cmd/clb/antc-cmn.bat
+++ cmd/clb/antc-cmn.bat
@@ -6,9 +6,11 @@ if %ANT_HOME%.==. goto done
 
 call vars-clb-cmn.bat
 
+:: TODO: remove these
 set CLB_EXTRA_DEFS="-Dvs.base=D:/Program Files (x86)/Microsoft Visual Studio 10.0"
 set CLB_EXTRA_DEFS=%CLB_EXTRA_DEFS% "-Dsdk.base=C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A"
 set CLB_EXTRA_DEFS=%CLB_EXTRA_DEFS% "-Dhelp.workshop.base=C:/Program Files (x86)/HTML Help Workshop"
+
 set CLB_EXTRA_DEFS=%CLB_EXTRA_DEFS% "-Djava.base=%JAVA_HOME%"
 set ANT_OPTS=%ANT_OPTS% %CLB_EXTRA_DEFS% -Djsse.enableSNIExtension=false
 
