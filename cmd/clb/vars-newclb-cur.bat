4c3ac1ad8ec13a909c1f8ba14c2431c6473e9d72
Ihar Hancharenka <iigoncharenko@mail.ru>
2018-01-30 18:08:05 +0300

    autocommit 2018-01-30 18:08:03

--- cmd/clb/vars-newclb-cur.bat
+++ cmd/clb/vars-newclb-cur.bat
@@ -17,12 +17,14 @@ set BUILD_TYPE=nlp
 set CLB_DEFS_CMP_VER=-Pfx.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.english.version=+
 :: ...
+set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.arabic.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.indonesian.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.russian.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.french.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.spanish.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.italian.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.portuguese.version=+
+set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.tagalog.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.thai.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.unknown.version=+
 :: ...
@@ -36,6 +38,6 @@ echo CLB_DEFS_CMP: %CLB_DEFS_CMP%
 ::echo after call vars-clb-cmn-after.bat !!!
 
 :: !!!
-::set CLB_GIT_SRC_ROOT=%CLB_SRC%\platform
-::set CLB_SRC_ROOT=%CLB_GIT_SRC_ROOT%
+set CLB_GIT_SRC_ROOT=%CLB_SRC%\platform
+set CLB_SRC_ROOT=%CLB_GIT_SRC_ROOT%
 call vars-git-p.bat
