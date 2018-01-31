54a50a1ceade8957b321f606ef54fe6fcf5e5a0a
Ihar Hancharenka <iigoncharenko@mail.ru>
2018-01-31 17:39:48 +0300

    autocommit 2018-01-31 17:39:45

--- cmd/clb/clb-b.bat
+++ cmd/clb/clb-b.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 set CTAGS_SRC=1
-set BUILD_FXLP=1
-call vars-newclb-cur.bat
+::set BUILD_FXLP=1
+call vars-clb-cur.bat
 
 call _print-choice-q.bat Build git CLB?
 set /P CHOICE_TYPE=Your choice: 
--- cmd/clb/vars-clb-cur.bat
+++ cmd/clb/vars-clb-cur.bat
@@ -1,6 +1,6 @@
 ::@echo off
 call vars-clb-cmn.bat
-set CLB_VER=main
+set CLB_VER=fx
 call vars-java8.bat
 call vars-antc8.bat
 call vars-gradle.bat
@@ -14,11 +14,10 @@ set CLB_SRC_ROOT=%CLB_SVN_SRC_ROOT%
 set BUILD_TYPE=continuous
 if %BUILD_FXLP%.==. goto skipFXLPbt
 set BUILD_TYPE=nlp
-set CLB_DEFS_CMP=%CLB_DEFS_CMP% -Pnlp.workspace=%CLB_SVN_SRC_ROOT%
-
 set CLB_DEFS_CMP_VER=-Pfx.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.english.version=+
 :: ...
+set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.arabic.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.indonesian.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.russian.version=+
 set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.french.version=+
@@ -33,12 +32,12 @@ set CLB_DEFS_CMP_VER=%CLB_DEFS_CMP_VER% -Plp.unknown.version=+
 :: 7.3.0.3
 :skipFXLPbt
 
-set CLB_DEFS_CMP=%CLB_DEFS_CMP% -Pbuild.type=%BUILD_TYPE%
-set CLB_DEFS_CMP=%CLB_DEFS_CMP% %CLB_DEFS_CMP_VER%
+set CLB_DEFS_CMP=%CLB_DEFS_CMP% -Pbuild.type=%BUILD_TYPE% -Pnlp.workspace=%CLB_SVN_SRC_ROOT% %CLB_DEFS_CMP_VER%
 echo CLB_DEFS_CMP: %CLB_DEFS_CMP%
 
 ::echo after call vars-clb-cmn-after.bat !!!
 
+:: !!!
 set CLB_GIT_SRC_ROOT=%CLB_SRC%\platform
 set CLB_SRC_ROOT=%CLB_GIT_SRC_ROOT%
 call vars-git-p.bat
