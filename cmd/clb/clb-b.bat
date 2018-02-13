48a58b5b4a200b354188f7a9c408c17cb42f0730
Ihar Hancharenka <iigoncharenko@mail.ru>
2018-02-13 18:42:51 +0300

    autocommit 2018-02-13 18:42:47

--- cmd/clb/clb-b.bat
+++ cmd/clb/clb-b.bat
@@ -4,13 +4,17 @@ set CTAGS_SRC=1
 set BUILD_FXLP=1
 call vars-clb-cur.bat
 
-call _print-choice-q.bat Build git CLB?
+call _print-choice-q.bat Build (%BUILD_FXLP%) git CLB?
 set /P CHOICE_TYPE=Your choice: 
 if %CHOICE_TYPE%.==q. goto lExit
 
 ::call clb-c-fx.bat
-::call clb-b-git-fxba.bat
-call clb-b-git-fx.bat
+
+if %BUILD_FXLP%.==. goto skipFXLP
+call clb-b-git-fxba.bat
+::call clb-b-git-fx.bat
+:skipFXLP
+
 ::call clb-b-cmp.bat
 
 :lExit
