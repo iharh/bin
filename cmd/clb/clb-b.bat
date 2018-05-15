a80c4d5a786ce8af28cbcc52da4e5242d363ab30
Ihar Hancharenka <iigoncharenko@mail.ru>
2018-05-15 17:32:51 +0300

    autocommit 2018-05-15 17:23:22

--- cmd/clb/clb-b.bat
+++ cmd/clb/clb-b.bat
@@ -8,9 +8,8 @@ call _print-choice-q.bat Build (%BUILD_FXLP%) git CLB?
 set /P CHOICE_TYPE=Your choice: 
 if %CHOICE_TYPE%.==q. goto lExit
 
-::call clb-c-fx.bat
-
 if %BUILD_FXLP%.==. goto skipFXLP
+::call clb-c-fx.bat
 call clb-b-git-fxba.bat
 call clb-b-git-fx.bat
 :skipFXLP
--- cmd/clb/clb-startup.bat
+++ cmd/clb/clb-startup.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-call vars-java8.bat
+::call vars-java8.bat
 call vars-clb-cmn.bat
 pushd %CLB_INST_ROOT%\server\bin
 call startup.bat
