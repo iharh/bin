8adba98b8d770312e4f9100de77ca50617b2f04a
Ihar Hancharenka <iigoncharenko@mail.ru>
2018-05-30 14:04:53 +0300

    autocommit 2018-05-30 14:04:49

--- cmd/util/shell/fzf.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-call vars-shell.bat
-%SHELL_ROOT%\fzf\fzf-0.15.6-windows_amd64.exe %*
-endlocal
--- powershell.bat
+++ powershell.bat
@@ -1,7 +1,1 @@
-@echo off
-setlocal
-::call vars-ecm-p.bat
-::set PROMPT=$p$_$$$S
-::%ComSpec% %*
-D:\dev\utils\PowerShell\6.0.2\pwsh.exe %*
-endlocal
+@D:\dev\utils\PowerShell\6.0.2\pwsh.exe %*
