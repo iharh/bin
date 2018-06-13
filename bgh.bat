f16074723b956848edec9025faf71c4e40a11c14
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-06-13 17:03:22 +0300

    autocommit 2018-06-13 17:03:15

--- bgh.bat
+++ bgh.bat
@@ -2,12 +2,11 @@
 setlocal
 call vars-ecm-p.bat
 
-call ss-copy.bat
+:: call ss-copy.bat
 call all-ap.bat 
 
-call ss-compr.bat
-call ss-trim.bat
+::call ss-compr.bat
+::call ss-trim.bat
 
 call yadisk-up.bat
 endlocal
-
