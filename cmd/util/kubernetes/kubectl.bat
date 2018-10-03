9fcdb11b539906acbbba094deffb49c476d755eb
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-10-03 18:15:35 +0300

    m

--- cmd/util/kubernetes/kubectl.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-call vars-utils.bat
-set MINIKUBE_BIN=%UTILS_ROOT%\kubectl
-set KUBECONFIG="C:\Users\Ihar_Hancharenka\.kube\config"
-%MINIKUBE_BIN%\kubectl.exe %*
-endlocal
--- cmd/util/kubernetes/minikube.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-call vars-utils.bat
-set MINIKUBE_BIN=%UTILS_ROOT%\minikube
-%MINIKUBE_BIN%\minikube-windows-amd64.exe %*
-endlocal
