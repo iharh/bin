b2cd723ac1d92322674d44d8fdcd5fe8577479a1
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-01-16 15:03:58 +0300

    m

--- cmd/clb/vars-clb-cmn.bat
+++ cmd/clb/vars-clb-cmn.bat
@@ -1,11 +1,5 @@
 @echo off
 set CLB_ROOT=D:\clb
-
 set CLB_SRC=%CLB_ROOT%\src
-
 set CLB_INST_ROOT=%CLB_ROOT%\inst
-
-::set HUDSON_SRC=\\epbygomw0039t1\integration
-::set HUDSON_DST=%CLB_ROOT%\hudson-integration
-
 set CLB_DIST_ROOT=%CLB_ROOT%\dist
