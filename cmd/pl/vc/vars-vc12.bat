f170866b5975cc2caf40932ed1299cfbd2e5cfda
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-07-16 20:33:32 +0300

    m

---
+++ cmd/pl/vc/vars-vc12.bat
@@ -0,0 +1,2 @@
+@echo off
+call "D:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64\vcvars64.bat"
---
+++ cmd/pl/vc/vars-vc14.bat
@@ -0,0 +1,2 @@
+@echo off
+call "D:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
