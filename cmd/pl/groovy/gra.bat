f30e2ce5463b59619b4932cf6c0b5a5dd9018974
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-07-19 19:17:15 +0300

    m

--- cmd/pl/groovy/gra.bat
+++ cmd/pl/groovy/gra.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal
 set GRADLE_USER_HOME=D:\.gradle
+set CB_NLP_TENSORFLOW_SYN_HOST=localhost
+set CB_NLP_TENSORFLOW_SYN_PORT=0
 ::call vars-java8.bat
 ::set GRADLE_HOME=%PL_ROOT%\groovy\tools\gradle-4.6
 ::call %GRADLE_HOME%\bin\gradle.bat %*
