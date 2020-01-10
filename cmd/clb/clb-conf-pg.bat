29c4acaae1b8baa38dfb990b3146d7fb06f78592
Ihar Hancharenka <iigoncharenko@gmail.com>
2020-01-10 17:32:45 +0300

    m

--- cmd/clb/clb-conf-pg.bat
+++ cmd/clb/clb-conf-pg.bat
@@ -1,17 +1,18 @@
 @echo off
 setlocal
 call vars-clb-cur.bat
+call vars-utils.bat
 
 call _print-choice-q.bat Configure PG DB ?
 
 set /P CHOICE_TYPE=Your choice: 
 if %CHOICE_TYPE%.==q. goto done
 
-call gradle.bat -b "D:/dev/prj/gra/kts/clb/build.gradle.kts" clbCfg
+call %UTILS_ROOT%\scoop\apps\gradle\current\bin\gradle.bat -b "D:/dev/prj/gra/kts/clb/build.gradle.kts" clbCfg
 call %~dp0.clb\cfg-win\clb-i-confpg.bat
 call %~dp0.clb\pg\clb-fillpg-win.bat
 
-::call %~dp0clb-un-svc.bat
-mklink /d %CLB_INST_ROOT%\scripts\groovy %CLB_SRC_ROOT%\cbtests\tests\groovy
+::::call %~dp0clb-un-svc.bat
+::mklink /d %CLB_INST_ROOT%\scripts\groovy %CLB_SRC_ROOT%\cbtests\tests\groovy
 :done
 endlocal
